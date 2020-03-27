f8cc131dcb4c849ac054b8797c92e370bb33dfc2
Finn Rietz <19836078+frietz58@users.noreply.github.com>
2020-03-27 11:48:37 +0100

    add comments

--- pepper_ws/src/pepper_moveit_setup.sh
+++ pepper_ws/src/pepper_moveit_setup.sh
@@ -1,3 +1,4 @@
+# add dependencies from source
 git clone https://github.com/ros-naoqi/pepper_moveit_config.git
 git clone https://github.com/ros-naoqi/pepper_robot.git
 git clone https://github.com/ros-naoqi/pepper_meshes.git
@@ -5,16 +6,22 @@ git clone https://github.com/ros-simulation/gazebo_ros_pkgs.git -b melodic-devel
 sudo apt-get install ros-melodic-moveit
 sudo apt-get install -y libgazebo9-dev
 
+# compile them
 cd ..
 catkin_make
+
+# start custom installation of pepper meshes
 cd build/
 make pepper_meshes_meshes
 cd ..
+
+# hacky but only way I got the meshes to load correctly
 cp -r devel/tmp/meshes src/pepper_meshes/
 
-# rosdep install --from-paths . --ignore-src --rosdistro melodic -y
+# attempts to install any still missing dependency
+rosdep install --from-paths . --ignore-src --rosdistro melodic -y
 
-
+# source development environment
 source devel/setup.bash
 
 
